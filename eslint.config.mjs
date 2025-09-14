c46061ba2992981fb0e80d546aa140fde23b2c56
bang <920673805@qq.com>
2025-09-15 00:15:51 +0800

    eslint和prettier调整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,40 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import prettier from "eslint-config-prettier"
+import pluginPrettier from "eslint-plugin-prettier"
 
-export default [{
-    files: ["**/*.ts"],
-}, {
+export default [
+  {
+    files: ["**/*.ts", "eslint.config.mjs"]
+  },
+  // eslint.configs.recommended,
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
+      prettier: pluginPrettier
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module"
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"]
+        }
+      ],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+      semi: "warn",
+      "prettier/prettier": "error"
+    }
+  },
+  prettier
+]
