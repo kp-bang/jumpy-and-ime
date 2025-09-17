2f8a9d94921ec2b66a56780f9a2ad0fa33bd51eb
bang <920673805@qq.com>
2025-09-18 07:03:13 +0800

    优化jumplines性能

--- src/utils/jumpy-lines.ts
+++ src/utils/jumpy-lines.ts
@@ -2,38 +2,23 @@ import * as vscode from "vscode"
 
 const jumpLines = (offset: number = 0) => {
   const editor = vscode.window.activeTextEditor
-  const { visibleRanges, document } = editor!
+  if (!editor) return
+  const { document } = editor
 
-  let visibleMinLine: number | undefined
-  let visibleMaxLine: number | undefined
-
-  let visibleLineNumbers = []
-  for (const range of visibleRanges) {
-    let lineNumber = range.start.line
-
-    while (lineNumber <= range.end.line) {
-      visibleLineNumbers.push(lineNumber++)
-
-      if (visibleMinLine === undefined || lineNumber < visibleMinLine) {
-        visibleMinLine = lineNumber
-      }
-      if (visibleMaxLine === undefined || visibleMaxLine < lineNumber) {
-        visibleMaxLine = lineNumber
-      }
+  const selections = editor.selections.map((sel) => {
+    const active = sel.active
+    let newLine: number
+    if (offset < 0) {
+      // 向上
+      newLine = Math.max(active.line - Math.abs(offset), 0)
+    } else {
+      // 向下
+      newLine = Math.min(active.line + offset, document.lineCount - 1)
     }
-  }
-
-  const centerLineNumberIndex = Math.floor(visibleLineNumbers.length / 2)
-  const centerLineNumber = visibleLineNumbers[centerLineNumberIndex]
-
-  const centerLine = document.lineAt(centerLineNumber)
-
-  vscode.window.activeTextEditor!.selection = new vscode.Selection(
-    centerLineNumber + offset,
-    centerLine.text.length,
-    centerLineNumber + offset,
-    centerLine.text.length
-  )
+    const newPos = new vscode.Position(newLine, active.character)
+    return new vscode.Selection(newPos, newPos) // 光标移动，无选区
+  })
+  editor.selections = selections
 
   const reviewType: vscode.TextEditorRevealType = vscode.TextEditorRevealType.InCenter
   vscode.window.activeTextEditor?.revealRange(vscode.window.activeTextEditor.selection, reviewType)
